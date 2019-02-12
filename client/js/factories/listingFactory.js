b117a774f8bd6fc9e67760daa45db875004f0b57
Kyler Clegg <kclegg101@hotmail.com>
2019-02-11 19:01:21 -0500

    update

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,15 +1,15 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('https://salty-everglades-33926.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('http://localhost:8080/api/listings/', listing);
+	  return $http.post('https://salty-everglades-33926.herokuapp.com/api/listings/', listing);
     },
 	
 	delete: function(_id) {
-	  return $http.delete('http://localhost:8080/api/listings/' + _id);
+	  return $http.delete('https://salty-everglades-33926.herokuapp.com/api/listings/' + _id);
     }
   };
 
